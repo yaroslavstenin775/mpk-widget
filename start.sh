327a459b4c5080bdc016151da0d175bb7a3b6f4e
Yaroslav <yaroslavstenin775@gmail.com>
2026-07-02 15:05:04 +0200

    Szybka aktualizacja rozkładu

--- start.sh
+++ start.sh
@@ -12,4 +12,4 @@ xset s off
 xset -dpms
 xset s noblank
 
-chromium-browser --disable-web-security --user-data-dir="/tmp/mpk_kiosk" --kiosk "file:///home/pi/mpk-widget/index.html"
+chromium-browser --disable-web-security --user-data-dir="/tmp/mpk_kiosk" --kiosk "file:///home/yaroslav/mpk-widget/index.html"
